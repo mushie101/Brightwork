50e64737cf3a025e8793b69880d9e963b3f3fba5
Mustafa Siddiqui <mustafa.ar.siddiqui@gmail.com>
2019-11-26 10:33:57 +0530

    Basic matplot

--- code/matplotpractice.py
+++ code/matplotpractice.py
@@ -2,5 +2,7 @@ from matplotlib import pyplot as mplt
 import numpy as np 
 a = np.array([1, 2, 3, 4, 5])
 b = np.array([5, 6, 7, 8, 9])
-mplt.plot(a, b)
+# mplt.plot(a, b)  Plotting a set of coordinates against another
+# mplt.hist(a, b) Plotting a histogram
+# mplt.scatter(a, b) Plotting a scatter plot
 mplt.show()
